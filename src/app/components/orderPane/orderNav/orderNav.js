468d18abea94c05e18eff71c1568b6422f4e149b
21kaushal <kaushal@proplco.com>
2017-06-05 19:40:21 +0530

    order service add with order Nav

--- src/app/components/orderPane/orderNav/orderNav.js
+++ src/app/components/orderPane/orderNav/orderNav.js
@@ -1,10 +1,36 @@
-function orderNavController($scope, $log) {
-  this.style = ['all', 'dine-in', 'delivery', 'multi'];
+function orderNavController($scope, $log, $rootScope, orderService) {
+  this.style = ['all', 'inStore', 'delivery', 'multi'];
+  this.text = 'Order Navigation panel';
+  var vo = this;
+  $scope.$on('getorders', function (event, style) {
+    var orderStyle = style;
+    vo.id = [];
+    vo.state = [];
+    vo.total = [];
+    angular.forEach(orderService.getData(), function (element) {
+      if (orderStyle === element.style) {
+        vo.id.push(element);
+        vo.state.push(element);
+      }
+    });
+  });
   this.selectTab = function (style) {
     $log.log(style);
-    $scope.$emit('getorders', style);
+    $rootScope.$broadcast('getorders', style);
   };
 }
+/* *
+this.putOrder = function(style) {
+  orderService.getData()
+  .then(function(response){
+    $log.log(response.data[0]);
+    vm.orders = response.data;
+  }).catch(function(error){
+    $log.error(error);
+  });
+};
+ * */
+
 module.exports = {
   template: require('./orderNav.html'),
   controller: orderNavController
