b7da3716cf104ea59ae1c8aeaf6ad9eb1d98fdf1
Ash Patel <ashharishpatel@gmail.com>
2017-06-08 19:54:31 +0530

    added item into the cart with quantity increasing with number of clicks on the item in productcategory

--- src/app/components/productCart/productCart.js
+++ src/app/components/productCart/productCart.js
@@ -6,9 +6,15 @@ function productCartController($scope, $log) {
   $scope.$on('itemObj', function (event, obj) {
     var id = obj.id;
     $log.log('recieved in cart ' + obj.id);
-
-    if (vm.cart) {
-      
+    var item = _.find(vm.cart, {id: obj.id});
+    if (item) {
+      item.qty++;
+    } else {
+      vm.cart.push({
+        id: obj.id,
+        name: obj.name,
+        qty: 1
+      });
     }
   });
 
